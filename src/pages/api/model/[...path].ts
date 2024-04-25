9c67d643f90fd90ab1674a1a12ddb98bce3347af
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-04-25 16:03:39 +0800

    update

--- src/pages/api/model/[...path].ts
+++ src/pages/api/model/[...path].ts
@@ -1,5 +1,5 @@
 import { getAuth } from "@clerk/nextjs/server";
-import { withPresets } from "@zenstackhq/runtime";
+import { enhance } from "@zenstackhq/runtime";
 import { NextRequestHandler } from "@zenstackhq/server/next";
 import type { NextApiRequest } from "next";
 import { prisma } from "../../../server/db/client";
@@ -8,7 +8,7 @@ async function getPrisma(req: NextApiRequest) {
   const auth = getAuth(req);
   // create a wrapper of Prisma client that enforces access policy,
   // data validation, and @password, @omit behaviors
-  return withPresets(prisma, { user: auth ? { id: auth.userId } : undefined });
+  return enhance(prisma, { user: auth ? { id: auth.userId } : undefined });
 }
 
 export default NextRequestHandler({ getPrisma });
